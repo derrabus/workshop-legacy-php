9fb5f66fe507e1db88af6703351c5343c5335680
Alexander M. Turek <me@derrabus.de>
2018-10-23 22:49:54 +0200

    Create a dedicated public directory.

--- public/app.php
+++ public/app.php
@@ -1,10 +1,12 @@
 <?php
 
+define('SCRIPTS_DIR', dirname(__DIR__).'/scripts');
+
 $targetFile = empty($_SERVER['REDIRECT_URL']) ? '/index.php' : $_SERVER['REDIRECT_URL'];
 if ($targetFile === '/') {
     $targetFile = '/index.php';
 }
-if (!file_exists(__DIR__.$targetFile)) {
+if (!file_exists(SCRIPTS_DIR.$targetFile)) {
     header('HTTP/1.1 404 Not Found');
     echo 'Not Found';
     exit;
@@ -19,6 +21,8 @@ $HTTP_GET_VARS = $_GET;
 $HTTP_POST_VARS = $_POST;
 $HTTP_COOKIE_VARS = $_COOKIE;
 
+chdir(SCRIPTS_DIR);
+
 ini_set('error_reporting', E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
 
-require __DIR__.$targetFile;
+require SCRIPTS_DIR.$targetFile;
