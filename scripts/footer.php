da1a0631d0b2d1c35be8b7fb6517ead00d8aa88e
Alexander M. Turek <me@derrabus.de>
2018-10-25 00:55:42 +0200

    Use a standard form login.

--- scripts/footer.php
+++ scripts/footer.php
@@ -10,13 +10,13 @@ $router = $container->get('router');
 <p>
     <a href="<?= $router->generate('legacy.home') ?>">Home</a> &mdash;
 <?php if (!$authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')): ?>
-    <a href="<?= $router->generate('legacy.login.php') ?>">Login</a>
+    <a href="<?= $router->generate('login') ?>">Login</a>
 <?php else: ?>
     <?php if ($authorizationChecker->isGranted('ROLE_ADMIN')): ?>
     <a href="<?= $router->generate('legacy.cat_admin.php') ?>">Manage categories</a> &mdash;
     <a href="<?= $router->generate('legacy.biz_admin.php') ?>">Add business</a> &mdash;
     <?php endif; ?>
     Logged in as <?= $container->get('security.token_storage')->getToken()->getUsername() ?>
-    (<a href="<?= $router->generate('legacy.logout.php') ?>">Logout</a>)
+    (<a href="<?= $router->generate('logout') ?>">Logout</a>)
 <?php endif; ?>
 </p>
